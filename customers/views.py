a14ee5e47c17302c54d0963b2f9199a51abdd5dd
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-04 03:22:56 +0300

    Created a single customer request function and added its path to urls.py

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,16 @@
 from customers.models import Customer
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
 
 def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
+
+def customer(request, id):
+    try:
+        data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        raise Http404('Customer Does not exist')
+    serializer = CustomerSerializer(data)
+    return JsonResponse({'customer': serializer.data})
