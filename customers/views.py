f90eb2098f3c0a2a673497aaed10a60801038592
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-08 02:55:08 +0300

    Intro to JWTs and Authentication (JSON web Tokens)

--- customers/views.py
+++ customers/views.py
@@ -1,11 +1,13 @@
 from customers.models import Customer
 from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def customers(request):
     if request.method == 'GET': 
         data = Customer.objects.all()        
@@ -20,6 +22,7 @@ def customers(request):
 
 
 @api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
