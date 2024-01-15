f7f47f13cc844a08d24e24fc4f11c42436eabc88
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-15 03:29:32 +0300

    GraphQL Mutations and Parameters in Graphene

--- customers/schema.py
+++ customers/schema.py
@@ -2,16 +2,40 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from customers.models import Customer
+import time
 
 class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
         fields = '__all__'
 
+class createCustomer(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        industry = graphene.String()
+    
+    customer = graphene.Field(CustomerType)
+
+    def mutate(root, info, name, industry):
+        customer = Customer(name=name, industry=industry)
+        customer.save()
+        return createCustomer(customer=customer)
+
+
 class Query(graphene.ObjectType):
     customers = graphene.List(CustomerType)
+    customer_by_name = graphene.List(CustomerType, name=graphene.String(required = True))
 
     def resolve_customers(root, info):
         return Customer.objects.all()
     
-schema = graphene.Schema(query=Query)
+    def resolve_customer_by_name(root, info, name):
+        try:
+            return Customer.objects.filter(name=name)
+        except Customer.DoesNotExist:
+            return None
+    
+class Mutations(graphene.ObjectType):
+    createCustomer = createCustomer.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutations)
